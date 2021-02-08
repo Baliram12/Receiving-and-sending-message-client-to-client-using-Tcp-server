397e92382f3b45b7f3a65b103320f392fc47398a
Baliram12 <balichauhan1997@gmail.com>
2021-02-08 10:59:35 +0530

    queue

--- client/go.mod
+++ client/go.mod
@@ -2,4 +2,4 @@ module client
 
 go 1.13
 
-require github.com/gookit/color v1.3.6
+require github.com/gookit/color v1.3.7
--- client/main.go
+++ client/main.go
@@ -3,62 +3,53 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io"
 	"log"
 	"net"
 	"os"
 	"strings"
-
-	"github.com/gookit/color"
 )
 
 func logFatal(err error) {
 	if err != nil {
 		log.Fatal(err)
-
 	}
-
 }
 
 func main() {
-	connection, err := net.Dial("tcp", "localhost:3000")
+
+	connection, err := net.Dial("tcp", "localhost:8080")
 	logFatal(err)
 
 	defer connection.Close()
-	color.Cyan.Println("Enter your Name[]:-")
+
+	fmt.Println("Enter username:")
 	reader := bufio.NewReader(os.Stdin)
 	username, err := reader.ReadString('\n')
 	logFatal(err)
 
 	username = strings.Trim(username, "\r\n")
-	welcomeMsg := fmt.Sprintf("[]:-Welcome %s,\n", username)
-	connection.Write([]byte(welcomeMsg))
+	welcomeMSg := fmt.Sprintf("[]:-Welcome %s ! Write here!! :-.", username)
+	fmt.Println(welcomeMSg)
 
-	color.Green.Println(welcomeMsg)
-	go read(connection)
-
-	write(connection, username)
+	go Read(connection)
+	Write(connection, username)
 
 }
 
-func read(connection net.Conn) {
+func Read(connection net.Conn) {
 	for {
-
 		reader := bufio.NewReader(connection)
 		message, err := reader.ReadString('\n')
-		if err == io.EOF {
-			connection.Close()
-			fmt.Println("connection closed")
-			os.Exit(0)
+		if err != nil {
+			fmt.Println(err)
 		}
-		color.Magenta.Println(message)
-		color.Red.Println("/*------------------------*---------------------*/")
+
+		fmt.Println(message)
 
 	}
-
 }
 
-func write(connection net.Conn, username string) {
+func Write(connection net.Conn, username string) {
 	for {
 		reader := bufio.NewReader(os.Stdin)
 		message, err := reader.ReadString('\n')
@@ -66,10 +57,8 @@ func write(connection net.Conn, username string) {
 			break
 		}
 
-		//formats the string to -> username:-message
-		message = fmt.Sprintf("[]:-%s:- %s\n", username, strings.Trim(message, "\r\n"))
+		message = fmt.Sprintf("%s:-%s\n", username, strings.Trim(message, "\r\n"))
 		connection.Write([]byte(message))
 
 	}
-
 }
